e5feb7c676f7731bbaf004a80a765b82e77a7259
xiangyuanding <92667653+xiangyuanding@users.noreply.github.com>
2023-08-09 00:14:42 -0700

    setup firestorehelper

--- src/screens/CreatePost.js
+++ src/screens/CreatePost.js
@@ -1,7 +1,9 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { writeToDB } from '../utils/FirestoreHelper'
 
 export default function CreatePost() {
+  writeToDB
   return (
     <View>
       <Text>CreatePost</Text>
--- src/utils/FirestoreHelper.js
+++ src/utils/FirestoreHelper.js
@@ -3,21 +3,21 @@ import { db } from "./firebase-setup";
 
 export async function deleteFromDB(id) {
   try {
-    await deleteDoc(doc(db, "entry", id));
+    await deleteDoc(doc(db, "posts", id));
   } catch (e) {
     console.log("delete error: ", e);
   }
 }
 export async function writeToDB(entry) {
   try {
-    const docRef = await addDoc(collection(db, "entry"), entry);
+    const docRef = await addDoc(collection(db, "posts"), entry);
   } catch (e) {
     console.log("write error: ", e);
   }
 }
 export async function setToDB(entry) {
   try {
-    const docRef = await setDoc(doc(db, "entry", entry.id), entry, {merge:true});
+    const docRef = await setDoc(doc(db, "posts", entry.id), entry, {merge:true});
   } catch (e) {
     console.log("Set error: ", e);
   }
